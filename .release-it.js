82b87cb91951e807e253f818297b09e93c3b043f
dimaslz <me@dimaslz.io>
2021-09-12 12:01:28 +0200

    chore(release): update after:bumb command

--- .release-it.js
+++ .release-it.js
@@ -2,7 +2,7 @@ module.exports = {
   "hooks": {
     "before:init": ["yarn test"],
     // "after:my-plugin:bump": "./bin/my-script.sh",
-    "after:bump": "NODE_ENV=production yarn build ng-v-class",
+    "after:bump": "NODE_ENV=production yarn build:lib",
     "after:git:release": "echo After git push, before github release",
     "after:release": "echo Successfully released ${name} v${version} to ${repo.repository}."
   },
